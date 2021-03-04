949ee204bf47c22d02f9752b8a5a85d5e53b97b2
RutOu <rutger.oudenampsen@student.hku.nl>
2021-03-04 16:42:21 +0100

    noGravMovement waterDetection

---
+++ Assets/Scripts/collideWithSurface.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collideWithSurface : MonoBehaviour
+{
+    public bool inWater = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("wSurface"))
+        {
+            Debug.Log("touch");
+            if (inWater == false)
+            {
+                inWater = true;
+                Debug.Log("Underwater");
+            } else {
+                inWater = false;
+                Debug.Log("Above water");
+            }
+        }
+    }
+}
