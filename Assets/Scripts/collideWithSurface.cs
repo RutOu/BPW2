1d86411ed562a2b974491e03128d31f2af3c2d3c
RutOu <rutger.oudenampsen@student.hku.nl>
2021-03-09 11:51:42 +0100

    movement switch delay: getting in and out of the water.

--- Assets/Scripts/collideWithSurface.cs
+++ Assets/Scripts/collideWithSurface.cs
@@ -8,27 +8,56 @@ public class collideWithSurface : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //checking for a collision with the watersurface.
         if (collision.CompareTag("wSurface"))
         {
             GameObject player = GameObject.Find("PlayerBody");
-
+            //checking if character is inside the water or not.
             Debug.Log("touch");
             if (inWater == false)
             {
                 inWater = true;
                 Debug.Log("Underwater");
-                player.GetComponent<PlayerMovWithGrav>().enabled = false;
-                player.GetComponent<PlayerMovNoGrav>().enabled = true;
+                IEnumerator WaitUnderWater()
+                {
+                    //Print the time of when the function is first called.
+                    Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+                    //yield on a new YieldInstruction that waits for a certain amount of seconds.
+                    yield return new WaitForSeconds(0.25f);
+                    //movement switch
+                    player.GetComponent<PlayerMovWithGrav>().enabled = false;
+                    player.GetComponent<PlayerMovNoGrav>().enabled = true;
+
+                    //After we have waited 5 seconds print the time again.
+                    Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+                }
+                StartCoroutine(WaitUnderWater());
             }
             else
             {
                 inWater = false;
                 Debug.Log("Above water");
-                player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 24f), ForceMode2D.Impulse);
-                player.GetComponent<PlayerMovNoGrav>().enabled = false;
-                player.GetComponent<PlayerMovWithGrav>().enabled = true;
-                
+                IEnumerator WaitAboveWater()
+                {
+                    //Print the time of when the function is first called.
+                    Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+                    //extra upward force
+                    player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 16f), ForceMode2D.Impulse);
+                    //yield on a new YieldInstruction that waits for a certain amount of seconds.
+                    yield return new WaitForSeconds(0.25f);
+                    //movement switch
+                    player.GetComponent<PlayerMovNoGrav>().enabled = false;
+                    player.GetComponent<PlayerMovWithGrav>().enabled = true;
+
+                    //After we have waited 5 seconds print the time again.
+                    Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+                }
+                StartCoroutine(WaitAboveWater());
             }
         }
     }
+    
+    
 }
