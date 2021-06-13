1f66512d2507d5214288d4ced81d5e85cfca0bac
RutOu <rutger.oudenampsen@student.hku.nl>
2021-06-13 14:36:13 +0200

    - drawn assets imported and positioned - camera follow player code added - colliders placed on correct assets - assets adjusted accordingly (added skyplanesprite, adjusted mistbordersprite)

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject PlayerHead;
+
+    private Vector3 offset; //Private variable to store offset distance between player and camera.
+
+    void Start()
+    {
+        //calculate and store offset value: distance between player's position and camera's postion.
+        offset = transform.position - PlayerHead.transform.position;
+    }
+
+    
+    void Update()
+    {
+        transform.position = PlayerHead.transform.position + offset;
+    }
+}
